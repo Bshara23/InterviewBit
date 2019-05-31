8eebcb7afd171df62d0920c978c18aeabdcd0d24
Bshara23 <bshara23@gmail.com>
2019-05-31 10:50:00 +0300

    s

--- interviewBit/tuplesExamples.py
+++ interviewBit/tuplesExamples.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
 	arr = [1, 2 ,3 ,4, 5]
 	
-	softCopy = arr[:] #soft copy of the array, no pointers
-	pointerCopy = arr #all changes made to pointerCopy is also applied to arr
+	softCopy = arr[:] # soft copy of the array, no pointers
+	pointerCopy = arr # all changes made to pointerCopy is also applied to arr
 
-	X = arr[1:]
-	Y = arr[:1]
-	Z = arr[1:2]
+	X = arr[1:] # [1,∞)
+	Y = arr[:1] # [0,1)
+	Z = arr[1:3] # [1:2)
 
-	print(X)
+	print(Z)
 	
