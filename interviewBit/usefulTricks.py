52434c1fdb4687f751603ba0e12a636ca684c105
Bshara23 <bshara23@gmail.com>
2019-06-02 21:08:04 +0300

    e

--- interviewBit/usefulTricks.py
+++ interviewBit/usefulTricks.py
@@ -9,3 +9,42 @@ if __name__ == "__main__":
 
     print(x)
     print(y)
+
+
+
+    
+    # initializing lists 
+    name = [ "Manjeet", "Nikhil", "Shambhavi", "Astha" ] 
+    roll_no = [ 4, 1, 3, 2 ] 
+    marks = [ 40, 50, 60, 70 ] 
+  
+    # using zip() to map values 
+    mapped = zip(name, roll_no, marks) 
+    mapped = set(mapped) 
+
+    print(mapped)
+
+
+    top2 = 20
+    top3 = 30
+    top1 = 10
+    top = 0
+    top = max(top,top1,top2,top3)
+
+
+    arr = [1, 2, 3, 2, -5, 6, 22, -4, 10]
+
+    # all values false
+    l = [0, False]
+    print(any(l))
+
+
+    # one true
+    l = [0, False, 1]
+    print(any(l))
+
+
+
+    # only 0 is false
+    l = [1, True, 4, -4, 0, 1]
+    print(all(l))
