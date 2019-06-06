732f14b6b105600e438919d4f2b6974d5b9c2003
Bshara23 <bshara23@gmail.com>
2019-06-06 10:49:23 +0300

    Update EqualAveragePartition.py

--- interviewBit/Problems/Dynamic Programming/EqualAveragePartition.py
+++ interviewBit/Problems/Dynamic Programming/EqualAveragePartition.py
@@ -1,22 +1,52 @@
 #https://www.interviewbit.com/problems/equal-average-partition/
-import itertools
 
-def getSets(A):
-    lenA = len(A)
-    setT = []
-    for i in range(1, lenA):
-        setT.append(list(map(set, itertools.combinations(A, i))))
+# couldn't solve it, solution based of interviewBit.com
+from functools import wraps
+import fractions
 
-    length = 0
-    for i in range(len(setT)):
-        length += len(setT[i])
-    print(length)
+def memo(f):
+    cache = {}
 
-    return setT
-
-
-#def avgset(A):
+    @wraps(f)
+    def wrap(*args):
+        if args not in cache:
+            cache[args] = f(*args)
+        return cache[args]
+    return wrap
 
 
-A = [1, 2, 3]
-print(getSets(A))
+class Solution:
+    # @param A : list of integers
+    # @return a list of list of integers
+
+    @memo
+    def knapsack(self, i, num, tot):
+        # Find num items in A that add up to tot
+        if i > len(self.A) - 1 or num <= 0 or tot <= 0:
+            return None
+        elif num == 1 and self.A[i] == tot:
+            return [self.A[i]]
+        else:
+            include = self.knapsack(i + 1, num - 1, tot - self.A[i])
+            exclude = self.knapsack(i + 1, num, tot)
+
+            if include:
+                return [self.A[i]] + include
+            elif exclude:
+                return exclude
+
+    def avgset(self, A):
+        tot = sum(A)
+        n = len(A)
+
+        gcd = fractions.gcd(tot, n)
+
+        num = n // gcd
+        self.A = sorted(A)
+
+        for i in range(num, n // 2 + 1, num):
+            k = self.knapsack(0, i, tot * i // n)
+            if k is not None:
+                temp = k[:]
+                return [k, [i for i in self.A if not i in temp or temp.remove(i)]]
+        return []
